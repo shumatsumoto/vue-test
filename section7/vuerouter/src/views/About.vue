e32ed9a53317fc39eda94f44dbc64c8af4851083
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-02 09:20:47 +0900

    route, router

--- section7/vuerouter/src/views/About.vue
+++ section7/vuerouter/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="checkRouteInfo">ルート情報</button>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: 'About',
+  methods: {
+    checkRouteInfo() {
+      console.log(this.$route)
+      console.log(this.$router)
+    }
+  }
+}
+</script>
