1462369fc39ed30afca6fd95def531993a4c2941
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-02 21:57:18 +0900

    vue params

--- section7/vuerouter/src/router/index.js
+++ section7/vuerouter/src/router/index.js
@@ -28,7 +28,12 @@ const routes = [
   {
     path: '/book/:id',
     name: 'Book',
-    component: BookDetail
+    component: BookDetail,
+    props: route => ({
+      id: route.params.id,
+      title: route.params.title,
+      content: route.params.content,
+    })
   }
 ]
 
