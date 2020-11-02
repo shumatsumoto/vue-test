9c52368ab1da48e31525e0daaef9efba2f104e00
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-02 21:22:25 +0900

    vue cli booklist

---
+++ section7/vuerouter/src/views/BookList.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		bookList
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BookList',
+	data() {
+		return {
+			bookIndex: -1,
+			books: [
+				{id: 1, title: 'タイトル1', content: '内容1'},
+				{id: 2, title: 'タイトル2', content: '内容2'},
+				{id: 3, title: 'タイトル3', content: '内容3'},
+			]
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
