3d3fe350ca1cb11b6e02efe2de85454e811d1906
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-06 11:38:28 +0900

    book search

--- section8/bookapp/src/pages/BookSearch.vue
+++ section8/bookapp/src/pages/BookSearch.vue
@@ -17,6 +17,27 @@
 					<v-btn color="secondary" to="/">一覧に戻る</v-btn>
 				</v-col>
 			</v-row>
+			<v-row>
+				<v-col cols="12" md="6" v-for="(book, index) in searchResults" :key="book.index">
+					<v-card class="mx-auto">
+						<v-row>
+							<v-col cols="4">
+								<v-img :src="book.image"></v-img>
+							</v-col>
+							<v-col cols="8">
+								<v-card-title>{{ book.title }}</v-card-title>
+								{{ book.description }}
+								<v-spacer></v-spacer>
+								<v-card-actions>
+									<v-btn class="mx-2" fab dark color="indigo" @click="addBookList(index)">
+										<v-icon dark>mdi-plus</v-icon>
+									</v-btn>
+								</v-card-actions>
+							</v-col>
+						</v-row>
+					</v-card>
+				</v-col>
+			</v-row>
 		</v-container>
 	</div>
 </template>
