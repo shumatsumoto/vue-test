2f0c2547a24bc5b9b430fa1cde2ecbedeb869712
Shu Matsumoto <shumatsumoto0328@gmail.com>
2020-11-06 15:52:26 +0900

    book edit

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -4,6 +4,7 @@
     <v-main>
       <v-container>
         <router-view
+          :books="books"
           @add-book-list="addBook"
          />
       </v-container>
@@ -52,6 +53,8 @@ export default {
       });
       // this.newBook = '';
       this.saveBooks();
+      // console.log(this.books.slice(-1)[0].id)
+      this.goToEditPage(this.books.slice(-1)[0].id)
     },
     removeBook(x) {
       this.books.splice(x, 1);
@@ -60,6 +63,9 @@ export default {
     saveBooks() {
       const parsed = JSON.stringify(this.books);
       localStorage.setItem(STORAGE_KEY, parsed);
+    },
+    goToEditPage(id) {
+      this.$router.push(`/edit/${id}`)
     }
   }
 };
--- section8/bookapp/src/pages/BookEdit.vue
+++ section8/bookapp/src/pages/BookEdit.vue
@@ -1,10 +1,13 @@
 <template>
-	<div>BookEdit</div>
+	<div>BookEdit {{ books }}</div>
 </template>
 
 <script>
 export default {
-
+	name: 'BookEdit',
+	props: {
+		books: Array
+	}
 }
 </script>
 
